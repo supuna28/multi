bde48549547b815262016ab55c255de0b7329aae
AlanX404 <76613101+alanwildan@users.noreply.github.com>
2021-12-09 10:47:06 +0700

    Add files via upload

---
+++ config.js
@@ -0,0 +1,38 @@
+const fs = require('fs')
+const chalk = require('chalk')
+
+// Website Api
+global.APIs = {
+	zenz: 'https://zenzapi.xyz',
+}
+
+// Apikey Website Api
+global.APIKeys = {
+	'https://zenzapi.xyz': 'Your Key',
+}
+
+// Other
+global.owner = ['6285793432434', '6285158410062']
+global.packname = 'Ecchi Senpai'
+global.author = '@shitpost.id_._'
+global.sessionName = 'sessions'
+global.prefa = ['/']
+global.sp = '⭔'
+global.mess = {
+    admin: 'Fitur Khusus Admin Group!',
+    botAdmin: 'Bot Harus Menjadi Admin Terlebih Dahulu!',
+    owner: 'Fitur Khusus Owner Bot',
+    group: 'Fitur Digunakan Hanya Untuk Group!',
+    private: 'Fitur Digunakan Hanya Untuk Private Chat!',
+    bot: 'Fitur Khusus Pengguna Nomor Bot',
+    wait: 'Loading...',
+}
+global.thumb = fs.readFileSync('./lib/puki.jpg')
+
+let file = require.resolve(__filename)
+fs.watchFile(file, () => {
+	fs.unwatchFile(file)
+	console.log(chalk.redBright(`Update'${__filename}'`))
+	delete require.cache[file]
+	require(file)
+})
